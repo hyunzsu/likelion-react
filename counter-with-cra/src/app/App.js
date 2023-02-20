3d23c733379f95a1d6a304a6f438009ceace34c0
hyunzsu <kkm5169@naver.com>
2023-02-20 17:12:10 +0900

    [Add] 카운터 클래스 컴포넌트

--- counter-with-cra/src/app/App.js
+++ counter-with-cra/src/app/App.js
@@ -1,29 +1,12 @@
 import './App.css';
-
-// 이미지 에셋 불러오기
-import logo from '../assets/logo.svg';
+import Counter from "../components/Counter";
 
 function App() {
-  console.log('rendering App Component');
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='React' />
-        <p>
-          <code>src/App.js</code> 파일을 수정하면{' '}
-          <abbr title="Hot Module Replacement">HMR</abbr>, Live Reload 됩니다.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          React 배우러 가자!
-        </a>
-      </header>
+      <Counter />
     </div>
-  );
+  )
 }
 
 export default App;
