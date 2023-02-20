d164d72f91e3ecd02bd205d64d601e68531295f0
hyunzsu <kkm5169@naver.com>
2023-02-20 15:59:17 +0900

    [Add] React 테스트 (TDD)

--- counter-with-cra/src/app/App.js
+++ counter-with-cra/src/app/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 이미지 에셋 불러오기
+import logo from '../assets/logo.svg';
 
 function App() {
   console.log('rendering App Component');
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img src={logo} className='App-logo' alt='React' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> 파일을 수정하면{' '}
+          <abbr title="Hot Module Replacement">HMR</abbr>, Live Reload 됩니다.
         </p>
         <a
           className='App-link'
@@ -16,7 +19,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          React 배우러 가자!
         </a>
       </header>
     </div>
